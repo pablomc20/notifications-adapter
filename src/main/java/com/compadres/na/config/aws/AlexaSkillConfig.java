fc8b05d8cdef8e7723a8afaaaed51dcfd1e372a0
Pablo Cruz Marquez <e_pcruz@bancoppel.io>
2026-04-13 17:50:17 -0600

    Add Serializer bean to AlexaSkillConfig for improved request handling. Refactor request handler registration for better readability.

--- src/main/java/com/compadres/na/config/aws/AlexaSkillConfig.java
+++ src/main/java/com/compadres/na/config/aws/AlexaSkillConfig.java
@@ -2,12 +2,11 @@ package com.compadres.na.config.aws;
 
 import com.amazon.ask.Skill;
 import com.amazon.ask.Skills;
-import com.compadres.na.components.handlers.AmazonFallbackIntentHandler;
-import com.compadres.na.components.handlers.AmazonHelpIntentHandler;
-import com.compadres.na.components.handlers.AmazonStopCancelIntentHandler;
-import com.compadres.na.components.handlers.GetNotificationsIntentHandler;
-import com.compadres.na.components.handlers.LaunchRequestHandlerImpl;
-import com.compadres.na.components.handlers.SessionEndedRequestHandlerImpl;
+import com.amazon.ask.dispatcher.request.handler.RequestHandler;
+import com.amazon.ask.model.services.Serializer;
+import com.amazon.ask.model.services.util.JacksonSerializer;
+
+import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,23 +17,15 @@ public class AlexaSkillConfig {
     @Bean
     public Skill alexaSkill(
             AlexaProperties alexaProperties,
-            LaunchRequestHandlerImpl launchRequestHandler,
-            GetNotificationsIntentHandler getNotificationsIntentHandler,
-            SessionEndedRequestHandlerImpl sessionEndedRequestHandlerImpl,
-            AmazonFallbackIntentHandler amazonFallbackIntentHandler,
-            AmazonStopCancelIntentHandler amazonStopCancelIntentHandler,
-            AmazonHelpIntentHandler amazonHelpIntentHandler) {
+            List<RequestHandler> handlers) {
         return Skills.standard()
-                .addRequestHandlers(
-                    launchRequestHandler,
-                    getNotificationsIntentHandler,
-                    sessionEndedRequestHandlerImpl,
-                    amazonFallbackIntentHandler,
-                    amazonHelpIntentHandler,
-                    amazonStopCancelIntentHandler
-                )
+        .addRequestHandlers(handlers.toArray(new RequestHandler[0]))
                 .withSkillId(alexaProperties.getSkillId())
                 .build();
     }
 
+    @Bean
+    public Serializer serializer() {
+        return new JacksonSerializer();
+    }
 }
